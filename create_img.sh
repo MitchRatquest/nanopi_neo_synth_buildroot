4aeef282825788e481d2a80def5b5cbb153d26c8
Mitchell Dokken <mitchell.dokken@gmail.com>
2017-05-25 20:13:56 -0500

    permissions

--- create_img.sh
+++ create_img.sh
@@ -6,5 +6,9 @@ tar zvxf buildroot-"$VERSION".tar.gz
 
 patch -p1 < 0000-add-pd-tk.patch 
 
+chmod+x nanopi_neo_synth/post-image.sh
+chmod+x nanopi_neo_synth/post-build.sh
+chmod+x nanopi_neo_synth/S45ntpdate
+
 make O=$PWD -C buildroot-2017.05-rc2/ defconfig BR2_DEFCONFIG=../nanopi_neo_synth_buildroot_defconfig
 make
