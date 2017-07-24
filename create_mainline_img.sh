dfbcb56d9fa0a654894483336c728d415a3f46eb
Mitchell Dokken <mitchell.dokken@gmail.com>
2017-07-23 20:01:46 -0500

    directory restructuring

--- create_mainline_img.sh
+++ create_mainline_img.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 VERSION=2017.05-rc2
 
-wget https://git.busybox.net/buildroot/snapshot/buildroot-"$VERSION".tar.gz
-tar zvxf buildroot-"$VERSION".tar.gz
+if [ ! -d buildroot-"$VERSION" ]
+then
+	wget https://git.busybox.net/buildroot/snapshot/buildroot-"$VERSION".tar.gz
+	tar zvxf buildroot-"$VERSION".tar.gz
+fi
 
 patch -p1 < 0000-add-pd-tk.patch 
 
@@ -10,5 +13,5 @@ chmod 755 nanopi_neo_synth/post-image.sh
 chmod 755 nanopi_neo_synth/post-build.sh
 chmod 755 nanopi_neo_synth/S45ntpdate
 
-make O=$PWD -C buildroot-2017.05-rc2/ defconfig BR2_DEFCONFIG=../configs/nanopi_neo_synth_buildroot_defconfig
+make O=$PWD -C buildroot-2017.05-rc2/ defconfig BR2_DEFCONFIG=../configs/mainline/buildroot_defconfig
 make
