d20f0337263e5240432c18f242fff7a5e0d2b26c
Mitchell Dokken <mitchell.dokken@gmail.com>
2017-08-28 19:41:08 -0500

    Update create_mainline_img.sh

--- create_mainline_img.sh
+++ create_mainline_img.sh
@@ -9,9 +9,5 @@ fi
 
 patch -p1 < 0000-add-pd-tk.patch 
 
-chmod 755 configs/mainline/post-image.sh
-chmod 755 configs/mainline/post-build.sh
-chmod 755 configs/mainline/S45ntpdate
-
 make O=$PWD -C buildroot-2017.05-rc2/ defconfig BR2_DEFCONFIG=../configs/mainline/buildroot_defconfig
 make
