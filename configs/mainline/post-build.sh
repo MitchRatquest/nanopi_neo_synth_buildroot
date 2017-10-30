ae693f0cb39c8d43cdd57f3a933effb7e4b58263
MitchRatquest <mitchell.dokken@gmail.com>
2017-10-30 10:07:32 -0500

    deleted:    ..config.tmp 	deleted:    .br-external.mk 	deleted:    .config 	deleted:    .config.old 	deleted:    configs/mainline/S80dhcp-server 	modified:   configs/mainline/buildroot_defconfig 	modified:   configs/mainline/busybox.config 	deleted:    configs/mainline/dhcpd.conf 	new file:   configs/mainline/dnsmasq.conf 	modified:   configs/mainline/genimage.cfg 	modified:   configs/mainline/linux_defconfig_synth.config 	modified:   configs/mainline/post-build.sh 	modified:   nanopi_buildroot.zip

--- configs/mainline/post-build.sh
+++ configs/mainline/post-build.sh
@@ -9,15 +9,12 @@ BOOT_CMD="${BOARD_DIR}/boot.cmd"
 BOOT_CMD_H="${BINARIES_DIR}/boot.scr"
 
 #copy configuration files to image
-cp $BOARD_DIR/S45ntpdate $TARGET_DIR/etc/init.d
-cp $BOARD_DIR/asound.conf $TARGET_DIR/etc
-cp $BOARD_DIR/interfaces $TARGET_DIR/etc/network
-#cp $BOARD_DIR/sun8i-h3-nanopi-neo.dtb $IMAGE_DIR/sun8i-h3-nanopi-neo.dtb
-cp $BOARD_DIR/dhcpd.conf $TARGET_DIR/etc/dhcp
-cp $BOARD_DIR/S80dhcp-server $TARGET_DIR/etc/init.d
-cp $BOARD_DIR/S41usb $TARGET_DIR/etc/init.d
-#ln -s $TARGET_DIR/usr/bin/wish8.6 $TARGET_DIR/usr/bin/wish
-mv $TARGET_DIR/etc/init.d/S40network $TARGET_DIR/etc/init.d/network-disabled
-
+cp $BOARD_DIR/S45ntpdate 		$TARGET_DIR/etc/init.d
+cp $BOARD_DIR/asound.conf 		$TARGET_DIR/etc
+cp $BOARD_DIR/interfaces	 	$TARGET_DIR/etc/network
+cp $BOARD_DIR/S41usb 			$TARGET_DIR/etc/init.d
+mv $TARGET_DIR/etc/init.d/S40network	$TARGET_DIR/etc/init.d/network-disabled
+cp $TARGET_DIR/usr/bin/wish8.6 		$TARGET_DIR/usr/bin/wish
+mv $BOARD_DIR/dnsmasq.conf		$TARGET_DIR/etc/
 # U-Boot script
 "${MKIMAGE}" -C none -A arm -T script -d "${BOOT_CMD}" "${BOOT_CMD_H}"
