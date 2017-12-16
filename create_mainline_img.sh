27523edab3329abe0d2b5a0e11f7fe85b4c4b74e
Mitchell Dokken <mitchell.dokken@gmail.com>
2017-12-16 13:51:00 -0600

    apply patch correctly

--- create_mainline_img.sh
+++ create_mainline_img.sh
@@ -7,7 +7,10 @@ then
 	tar zvxf buildroot-"$VERSION".tar.gz
 fi
 
-patch -p1 < 0000-add-pd-tk.patch 
+cd buildroot-"$VERSION"
+patch -p1 < ../patches/buildroot/0000-add-pd-tk.patch
+cd ..
+
 
 make O=$PWD -C buildroot-2017.05-rc2/ defconfig BR2_DEFCONFIG=../configs/mainline/buildroot_defconfig
 make
